526e2356c82dfa6b6fec4af073ed657d004d89be
Hampus2001 <hampus.soderlund@chasacademy.se>
2025-04-08 15:07:40 +0200

    added react imports

--- frontend/app/me/[userId]/page.js
+++ frontend/app/me/[userId]/page.js
@@ -1,5 +1,5 @@
 "use client";
-
+import React from "react";
 import { HandleAccountContext } from "@/context/AccountContext";
 import { useContext, useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
