5cb24fb253439551b4b5a23c7a9584fd0e7d9c38
Hampus2001 <hampus.soderlund@chasacademy.se>
2025-04-08 11:53:37 +0200

    deploy test

--- frontend/app/loginPage/page.test.js
+++ frontend/app/loginPage/page.test.js
@@ -2,14 +2,22 @@ import React from "react";
 import { render, fireEvent, screen } from "@testing-library/react";
 import LoginPage from "./page";
 
+jest.mock("next/navigation", () => ({
+  useRouter: () => ({
+    push: jest.fn(),
+  }),
+}));
+
 global.fetch = jest.fn(() =>
-  Promise.resolve({ ok: true, json: () => Promise.resolve([{ id: 1 }]) })
+  Promise.resolve({
+    ok: true,
+    json: () => Promise.resolve([{ id: 1 }]),
+  })
 );
 
 test("calls login with correct data", async () => {
   render(<LoginPage />);
 
-  // Simulate user input
   fireEvent.change(screen.getByPlaceholderText("username"), {
     target: { value: "testUser" },
   });
@@ -17,10 +25,8 @@ test("calls login with correct data", async () => {
     target: { value: "testPassword" },
   });
 
-  // Simulate button click
   fireEvent.click(screen.getByText("Log in"));
 
-  // Assert fetch was called with correct arguments
   expect(fetch).toHaveBeenCalledWith("http://13.60.77.158:3001/login", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
