779c1ca15acf8a19443f2580fcf4d88846e53cf6
Hampus2001 <hampus.soderlund@chasacademy.se>
2025-04-08 12:02:12 +0200

    added testing-library

--- frontend/app/page.test.js
+++ frontend/app/page.test.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom"; // Import jest-dom matchers
 import Home from "./page";
 import { useRouter } from "next/navigation";
 
